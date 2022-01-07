89748796dcdd1fd151933bc58530e20354f0f3bb
Venkat Shiva Reddy <venkat@pratian.com>
2022-01-07 15:15:38 +0530

    xml

--- Linq2Objects/Program.cs
+++ Linq2Objects/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Linq2Objects
 {
@@ -12,59 +13,98 @@ namespace Linq2Objects
         {
             //Example 1: Get All Products whose price is more than 80k
             ProductsRepository repo = new ProductsRepository();
-            var result1 = from p in repo.GetProducts()
-                          where p.Price >= 80000
-                          select p;
-            //todo: get with extention methods
+
+            // store all products into xml document
+            //XDocument xmlProducts = new XDocument("Products",
+            //        from p in repo.GetProducts()
+            //        select new XElement("Product",
+            //            new XElement("Name", p.Name),
+            //            new XElement("Price", p.Price),
+            //            new XElement("Catagory", p.Catagory.Name)));
 
 
-            foreach (var item in result1)
-            {
-                Console.WriteLine($"{item.Name}\t{item.Price}");
-            }
+            XDocument xmlProducts = new XDocument(new XElement("Products",
+                   from p in repo.GetProducts()
+                   select new XElement("Product", 
+                       new XElement("Name", p.Name),
+                       new XElement("Price", p.Price),
+                       new XElement("Catagory", 
+                           new XElement("CName",
+                            p.Catagory.Name)))));
 
-            // Example 2: Get all products in stock
-            var result2 = repo.GetProducts().Where(p => p.InStock);
-            result2 = from p in repo.GetProducts()
-                      where p.InStock == true
-                      select p;
+            xmlProducts.Save("d:\\products.xml");
 
-            // Example 3: Get only the name of all products
-            var result3 = from p in repo.GetProducts()
-                          select p.Name;
-
-            //todo: get with extention methods
+            Console.WriteLine(xmlProducts);
+                
+            //var result1 = from p in repo.GetProducts()
+            //              where p.Price >= 80000
+            //              select p;
+            ////todo: get with extention methods
 
 
-            // Lab 4: Get only the name and price of all the products
+            //foreach (var item in result1)
+            //{
+            //    Console.WriteLine($"{item.Name}\t{item.Price}");
+            //}
 
-            // Lab 5: Get total price of all the products in stock
+            //// Example 2: Get all products in stock
+            //var result2 = repo.GetProducts().Where(p => p.InStock);
+            //result2 = from p in repo.GetProducts()
+            //          where p.InStock == true
+            //          select p;
 
-            // Lab 6: Get total count of all the prudcts whose price is less than 50k
+            //// Example 3: Get only the name of all products
+            //var result3 = from p in repo.GetProducts()
+            //              select p.Name;
 
-            // Lab 7: Get Name of the product and Name of the catagory
+            ////todo: get with extention methods
 
-            // Lab 8: Get all the products belogns to Laptops catagory
 
-            // Lab 9: Get all the products total price belogs to mobiels
+            //// Lab 4: Get only the name and price of all the products
 
-            // Lab 10: Get all the products average price belongs to Apple brand
+            //var result4 = from p in repo.GetProducts()
+            //              select new  { Name =p.Name, Price=p.Price };
 
-            // Lab 11: Show the costliest product name
+
+
+            //// Lab 5: Get total price of all the products in stock
+
+            //// Lab 6: Get total count of all the prudcts whose price is less than 50k
+
+            //// Lab 7: Get Name of the product and Name of the catagory
+            //var result7 = from p in repo.GetProducts()
+            //              select new {PName= p.Name,CName = p.Catagory.Name };
+
+
+
+            //// Lab 8: Get all the products belogns to Laptops catagory
+
+            //// Lab 9: Get all the products total price belogs to mobiels
+
+            //// Lab 10: Get all the products average price belongs to Apple brand
+
+            //// Lab 11: Show the costliest product name
+
+            //// Lab 12: Show the cheapest product name and its price
+
+            //// Lab 13: Show the 2nd and 3rd product details
+
+            //var result13 = repo.GetProducts().Skip(1).Take(2);
             
-            // Lab 12: Show the cheapest product name and its price
+            //// Lab 14: List all products in assending order of their price
             
-            // Lab 13: Show the 2nd and 3rd product details
+            //// Lab 15: Show which catagory has costly product
             
-            // Lab 14: List all products in assending order of their price
-            
-            // Lab 15: Show which catagory has costly product
-            
-            // Lab 16: Show which catagory has less products
+            //// Lab 16: Show which catagory has less products
 
         }
     }
 
+    //class NamePrice
+    //{
+    //    public string Name { get; set; }
+    //    public int Price { get; set; }
+    //}
 
     class ProductsRepository
     {
---
+++ Linq2XMLDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Linq2XMLDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> words = new List<string> {"one","two","three","four","five","six" };
+
+            // select all short words (<=3)
+
+            XDocument xmlShortWords = new XDocument(
+                new XElement("words", 
+                            from w in words
+                            where w.Length <= 3
+                            select new XElement("word",w)));
+
+            xmlShortWords.Save("d:\\words.xml");
+
+            //foreach (var item in shortWords)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            // LINQ to XML
+
+            // Load xml document
+            //XDocument xml = XDocument.Load("XMLFile1.xml");
+
+            //var xmlShortWords = from x in xml.Descendants("word")
+            //                    where x.Value.Length <= 3
+            //                    select x.Value;
+
+            //foreach (var item in xmlShortWords)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+        }
+    }
+}
