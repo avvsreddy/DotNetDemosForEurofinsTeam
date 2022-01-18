14f582c77c584e28d0794cef7129299807936a1c
Venkat Shiva Reddy <venkat@pratian.com>
2022-01-18 12:35:45 +0530

    api client

---
+++ CoolProductsClientCatalog/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolProductsClientCatalog.Entities
+{
+    class Product
+    {
+      
+            public int ProductID { get; set; }
+            public string Name { get; set; }
+            public int Price { get; set; }
+            public string Brand { get; set; }
+            public bool InStock { get; set; }
+            public object Catagory_CatagoryId { get; set; }
+        }
+
+    }
+
---
+++ CoolProductsClientCatalog/Program.cs
@@ -0,0 +1,40 @@
+﻿using CoolProductsClientCatalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolProductsClientCatalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Discover the API Endpoint
+            // http://localhost:60905/api/coolproducts/instock
+            ShowAllProductsInStock();
+
+
+        }
+
+
+        public static void ShowAllProductsInStock()
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = httpClient.GetAsync("http://localhost:60905/api/coolproducts/instock").Result;
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                List<Product> products = response.Content.ReadAsAsync<List<Product>>().Result;
+
+                foreach (var item in products)
+                {
+                    Console.WriteLine(item.Name + "\t" + item.Price);
+                }
+            }
+            else
+                Console.WriteLine("No Products in stock");
+        }
+    }
+}
