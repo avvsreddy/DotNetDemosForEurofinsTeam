f7f2d000da00d0682052db749a9ad4c4ca93b992
Venkat Shiva Reddy <venkat@pratian.com>
2022-01-20 15:50:17 +0530

    Layouts Demo

---
+++ MySimpleCalculator/GridLayoutDemo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MySimpleCalculator
+{
+    /// <summary>
+    /// Interaction logic for GridLayoutDemo.xaml
+    /// </summary>
+    public partial class GridLayoutDemo : Window
+    {
+        public GridLayoutDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Add is clicked");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Subtract is clicked");
+        }
+    }
+}
